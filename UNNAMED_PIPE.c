03c73116a9ca051f29eec25ba783211e85cf65ac
Swetha Perumalsamy <144234049+swethaperumalsamy@users.noreply.github.com>
2026-06-24 10:06:58 +0530

    Create UNNAMED_PIPE.c

---
+++ UNNAMED_PIPE.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main()
+{
+    int fd[2];
+    char buffer[100];
+
+    pipe(fd);
+
+    if(fork() == 0)
+    {
+        read(fd[0], buffer, sizeof(buffer));
+        printf("Child received: %s\n", buffer);
+    }
+    else
+    {
+        char msg[] = "Hello Child";
+        write(fd[1], msg, strlen(msg)+1);
+    }
+
+    return 0;
+}
