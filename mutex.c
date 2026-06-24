43566252a8881153b2c4e4cf42e2fe802dc62e5f
Swetha Perumalsamy <144234049+swethaperumalsamy@users.noreply.github.com>
2026-06-24 09:59:05 +0530

    Create mutex.c

---
+++ mutex.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <pthread.h>
+
+pthread_mutex_t lock;
+
+void *display(void *arg)
+{
+    pthread_mutex_lock(&lock);
+    printf("Thread Executing\n");
+    pthread_mutex_unlock(&lock);
+
+    return NULL;
+}
+
+int main()
+{
+    pthread_t t1, t2;
+
+    pthread_mutex_init(&lock, NULL);
+
+    pthread_create(&t1, NULL, display, NULL);
+    pthread_create(&t2, NULL, display, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    pthread_mutex_destroy(&lock);
+
+    return 0;
+}
