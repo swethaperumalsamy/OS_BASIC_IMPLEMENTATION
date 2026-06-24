74ba87d184637a189dc8e0c81725cf84b02a0c54
Swetha Perumalsamy <144234049+swethaperumalsamy@users.noreply.github.com>
2026-06-24 10:24:32 +0530

    Create TCP_SOCKET.c

---
+++ TCP_SOCKET.c
@@ -0,0 +1,69 @@
+server.c
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main()
+{
+    int server_fd, client_fd;
+    char buffer[100];
+
+    struct sockaddr_in server;
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(8080);
+
+    bind(server_fd, (struct sockaddr *)&server, sizeof(server));
+
+    listen(server_fd, 1);
+
+    printf("Waiting for client...\n");
+
+    client_fd = accept(server_fd, NULL, NULL);
+
+    recv(client_fd, buffer, sizeof(buffer), 0);
+
+    printf("Received: %s\n", buffer);
+
+    close(client_fd);
+    close(server_fd);
+
+    return 0;
+}
+
+client.c
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main()
+{
+    int sockfd;
+
+    struct sockaddr_in server;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(8080);
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    connect(sockfd, (struct sockaddr *)&server, sizeof(server));
+
+    char msg[] = "Hello TCP";
+
+    send(sockfd, msg, strlen(msg) + 1, 0);
+
+    printf("Message Sent\n");
+
+    close(sockfd);
+
+    return 0;
+}
